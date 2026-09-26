a8ce4857305a901c8564416a192bf683f3fd8e45
Maximilian Mitchel <max@maxis.me>
2026-09-26 11:10:04 +0100

    The palette is black, white, dynamic, then the hues

--- TypeMeIt/CloudColor.swift
+++ TypeMeIt/CloudColor.swift
@@ -1,13 +1,20 @@
 import AppKit
 
-/// The recording cloud's colour when it does not follow the appearance.
+/// The recording cloud's colour when it is not dynamic: black, white, or
+/// one of seven hues.
 enum CloudColor: String, Codable, CaseIterable, Sendable {
-    case coral, amber, lemon, mint, sky, lavender, rose
+    case black, white, coral, amber, lemon, mint, sky, lavender, rose
+
+    /// The two a dynamic cloud chooses between.
+    static let neutrals: [CloudColor] = [.black, .white]
+    static var hues: [CloudColor] { allCases.filter { !neutrals.contains($0) } }
 
     var label: String { rawValue }
 
     var color: NSColor {
         switch self {
+        case .black: NSColor(white: 0.25, alpha: 1)
+        case .white: NSColor(white: 1, alpha: 1)
         case .coral: NSColor(srgbRed: 0.98, green: 0.49, blue: 0.40, alpha: 1)
         case .amber: NSColor(srgbRed: 0.98, green: 0.69, blue: 0.29, alpha: 1)
         case .lemon: NSColor(srgbRed: 0.96, green: 0.86, blue: 0.36, alpha: 1)
